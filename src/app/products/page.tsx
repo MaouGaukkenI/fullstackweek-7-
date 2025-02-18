bfa0fa0bdd3892e081303ea1dd3d8d6249ebf9f3
Diogo B.Beloni <gaukkenwork@gmail.com>
2025-02-18 20:42:51 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProducPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1>Products Page!</h1>;<Button>Button Teste</Button>;
+      <Input placeholder="Password" />
+    </div>
+  );
+};
+
+export default ProducPage;
