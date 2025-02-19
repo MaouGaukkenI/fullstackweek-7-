78cdaa95199b87c22f0426b6360847fd6e82c338
Diogo B.Beloni <gaukkenwork@gmail.com>
2025-02-18 21:19:24 -0300

    chore: update css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,7 +4,8 @@ import { Input } from "@/components/ui/input";
 const ProducPage = () => {
   return (
     <div className="p-5 border border-red-500 rounded-xl">
-      <h1>Products Page!</h1>;<Button>Button Teste</Button>;
+      <h1>Products Page!</h1>
+      <Button>Button Teste</Button>
       <Input placeholder="Password" />
     </div>
   );
